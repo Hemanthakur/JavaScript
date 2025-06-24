751b0cf0a1ea1ae5f5cf580a744820c3e7c51aae
Hemanthakur <nikhilt919@gmail.com>
2025-06-25 01:35:03 +0530

    Dom Manipulation Attribut and Styles

---
+++ DOM/attri.js
@@ -0,0 +1,19 @@
+// let p = document.querySelector("p");
+// console.log(p);
+
+// let clas = p.getAttribute("class");
+// console.log(clas);
+
+// let h = document.querySelector("h2");
+// let id = h.getAttribute("id");
+// console.log(id);
+
+let para = document.querySelector("p");
+console.log(para);
+console.log(para.setAttribute("class", "newClass"));
+
+let div = document.querySelector("div");
+div.style.backgroundColor = "red";
+div.style.color = "white";
+div.style.fontSize = "34px";
+div.innerText = "This is a new text for the div element.";
